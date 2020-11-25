ef406180d3e3753917e73524ec29037a75b0ef4e
Christian AS <alvitesscf18@uww.edu>
2020-11-24 23:02:17 -0600

    started php pagination

--- products.php
+++ products.php
@@ -20,7 +20,7 @@
             </div>       
             <nav>
                 <ul>
-                    <li><a href="index.php">Home</a></li>
+                    <li><a href="index.html">Home</a></li>
                     <li><a href="">Products</a></li>
                     <li><a href="#footer">About</a></li>
                     <li><a href="#footer">Contacts</a></li>
@@ -32,6 +32,27 @@
                 </div>
             </a>
         </header>
+    </div>
+   <!-- display products with php pagination -->
+   <div class="products">
+        <?php 
+            //define num of result per page
+            $results_per_page = 10;
+            
+            $sql_query = "SELECT * FROM products";
+            $result = mysqli_query($conn, $sql_query);
+            $number_of_results = mysqli_num_rows($result);
+            
+            while ($row = mysqli_fetch_array($result)) {
+                echo $row['name-desc'] . ' ' . $row['price'] . '<br>';
+            }
+            
+            $number_of_pages = ceil($number_of_results / $results_per_page);
+            
+            for ($page=1; $page <= $number_of_pages; ++$page) {
+                echo '<a href = "index2.php?page=' . $page . '">' . $page . '</a> ';
+            }
+        ?>
     </div>
 </body>
 </html>
